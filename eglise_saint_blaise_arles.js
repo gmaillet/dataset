ffd1d431669f110bbf8544f6880accd4269fbe92
gregoire maillet <gregoire.maillet@ign.fr>
2017-11-24 10:10:50 +0100

    created with PoTree

---
+++ eglise_saint_blaise_arles.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "eglise_saint_blaise_arles",
+    "projection": "EPSG:4978",
+    "points": 18380597,
+    "boundingBox": {
+        "lx": 4605344.880001068,
+        "ly": 373051.4399986267,
+        "lz": 4382088.710000038,
+        "ux": 4605407.470001221,
+        "uy": 373114.0299987793,
+        "uz": 4382151.300000191
+    },
+    "tightBoundingBox": {
+        "lx": 4605344.880001068,
+        "ly": 373051.4399986267,
+        "lz": 4382088.710000038,
+        "ux": 4605394.100001068,
+        "uy": 373114.02999862676,
+        "uz": 4382143.6100000389
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED"
+    ],
+    "spacing": 0.5420452952384949,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
